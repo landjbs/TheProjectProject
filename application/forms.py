b3fbebdb9198ffe77394eadfc618ccdc8bbe1058
Landon Smith <landjbs@gmail.com>
2020-07-09 18:55:15 -0400

    user class

--- application/forms.py
+++ application/forms.py
@@ -1,9 +1,22 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm, Form
 from wtforms import TextField, validators
+from wtforms_alchemy import model_form_factory
 from .models import Data
 
-class EnterDBInfo(Form):
-    dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])
+BaseForm = model_form_factory(FlaskForm)
+
+
+class EnterDBInfo(BaseForm):
+    class Meta:
+        model = Data
+        validators = {'name': [DataRequired()],
+                     'email': [DataRequired(), Email(), Length(min=1, max=254)],
+                     'password': [DataRequired(), Length(min=1, max=254)]}
+
+
+
+# class EnterDBInfo(Form):
+#     dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])
 
 class RetrieveDBInfo(Form):
     numRetrieve = TextField(label='Number of DB Items to Get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
--- application/models.py
+++ application/models.py
@@ -4,6 +4,9 @@ from application import db
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True, unique=False, info={'label':'Password'})
+    email = db.Column(db.String(254), unique=False, nullable=False,
+                      info={'label':'Email Address'})
+    # password
     password = Column(String(254), nullable=False, info={'label':'Password'})
 
     def __init__(self, name, password):
