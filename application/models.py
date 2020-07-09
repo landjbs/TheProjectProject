9d43b9f3dde93c7d7a49591cbca58211e817a893
Landon Smith <landjbs@gmail.com>
2020-07-09 18:52:02 -0400

    db init

--- application/models.py
+++ application/models.py
@@ -3,10 +3,13 @@ from application import db
 
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    notes = db.Column(db.String(128), index=True, unique=False)
+    name = db.Column(db.String(128), index=True, unique=False, info={'label':'Password'})
+    password = Column(String(254), nullable=False, info={'label':'Password'})
 
-    def __init__(self, notes):
-        self.notes = notes
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
 
     def __repr__(self):
         return '<Data %r>' % self.notes
+    
--- config.py
+++ config.py
@@ -3,13 +3,13 @@ db_password = 'ueIKdWZClnoRZLj6Wu0i'
 endpoint = 'collab-commons-rds.c4u7frshhdtj.us-east-1.rds.amazonaws.com:3306'
 db_url = 'collab-commons-rds'
 
-SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_password}@{endpoint}/{db_url}'
+# SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_password}@{endpoint}/{db_url}'
 
 # VCP ID: vpc-4d83fe37
 
 
 # Uncomment the line below if you want to work with a local DB
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 
