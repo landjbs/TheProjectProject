d1c3bc9a4e625bf799c6c2c0b2315f8371e7aae5
Landon Smith <landjbs@gmail.com>
2020-07-09 17:44:49 -0400

    rds connection

---
+++ config.py
@@ -0,0 +1,14 @@
+db_user = 'admin'
+db_password = 'ueIKdWZClnoRZLj6Wu0i'
+endpoint = 'collab-commons-db.c4u7frshhdtj.us-east-1.rds.amazonaws.com:3306'
+db_url = 'collab-commons-db'
+
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{db_user}:{db_password}@{endpoint}/{db_url}'
+
+# Uncomment the line below if you want to work with a local DB
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+# SQLALCHEMY_POOL_RECYCLE = 3600
+#
+# WTF_CSRF_ENABLED = True
+# SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
