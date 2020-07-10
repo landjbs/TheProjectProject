55b4768936ddeb67248c9078db5439a5bbbbec5e
Landon Smith <landjbs@gmail.com>
2020-07-10 18:42:03 -0400

    base restruct

--- application.py
+++ application.py
@@ -20,37 +20,41 @@ application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
-    form1 = Apply(request.form)
-    form2 = RetrieveDBInfo(request.form)
+    return render_template('index.html')
 
-    if request.method == 'POST' and form1.validate_on_submit():
-        data_entered = Data(name=form1.data['name'],
-                            email=form1.data['email'],
-                            password=form1.data['password'],
-                            github=form1.data['github'],
-                            about=form1.data['about'])
-        try:
-            db.session.add(data_entered)
-            db.session.commit()
-            db.session.close()
-        except Exception as e:
-            print(f'ERROR: {e}')
-            db.session.rollback()
-        return render_template('thanks.html', notes=form1.data['name'])
 
-    if request.method == 'POST' and form2.validate():
-        try:
-            num_return = int(form2.numRetrieve.data)
-            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-            for q in query_db:
-                print(f'NAME: {q.name}')
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('results.html', results=query_db,
-                               num_return=num_return)
-
-    return render_template('index.html', form1=form1, form2=form2)
+# def index():
+#     form1 = Apply(request.form)
+#     form2 = RetrieveDBInfo(request.form)
+#
+#     if request.method == 'POST' and form1.validate_on_submit():
+#         data_entered = Data(name=form1.data['name'],
+#                             email=form1.data['email'],
+#                             password=form1.data['password'],
+#                             github=form1.data['github'],
+#                             about=form1.data['about'])
+#         try:
+#             db.session.add(data_entered)
+#             db.session.commit()
+#             db.session.close()
+#         except Exception as e:
+#             print(f'ERROR: {e}')
+#             db.session.rollback()
+#         return render_template('thanks.html', notes=form1.data['name'])
+#
+#     if request.method == 'POST' and form2.validate():
+#         try:
+#             num_return = int(form2.numRetrieve.data)
+#             query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
+#             for q in query_db:
+#                 print(f'NAME: {q.name}')
+#             db.session.close()
+#         except:
+#             db.session.rollback()
+#         return render_template('results.html', results=query_db,
+#                                num_return=num_return)
+#
+#     return render_template('index.html', form1=form1, form2=form2)
 
 
 @application.route('/apply', methods=['GET', 'POST'])
