e89994823a334e65cc5766c559b0ed506125df7e
Landon Smith <landjbs@gmail.com>
2020-07-10 19:10:54 -0400

    login

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import (Flask, render_template, request, flash, redirect,
+                   url_for, session)
 from flask_login import current_user, login_user, logout_user, login_required
 
 from application import db
@@ -12,44 +13,11 @@ application.debug=True
 # change this to your own value
 application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 
+
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
-
-
-# def index():
-#     form1 = Apply(request.form)
-#     form2 = RetrieveDBInfo(request.form)
-#
-#     if request.method == 'POST' and form1.validate_on_submit():
-#         data_entered = Data(name=form1.data['name'],
-#                             email=form1.data['email'],
-#                             password=form1.data['password'],
-#                             github=form1.data['github'],
-#                             about=form1.data['about'])
-#         try:
-#             db.session.add(data_entered)
-#             db.session.commit()
-#             db.session.close()
-#         except Exception as e:
-#             print(f'ERROR: {e}')
-#             db.session.rollback()
-#         return render_template('thanks.html', notes=form1.data['name'])
-#
-#     if request.method == 'POST' and form2.validate():
-#         try:
-#             num_return = int(form2.numRetrieve.data)
-#             query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-#             for q in query_db:
-#                 print(f'NAME: {q.name}')
-#             db.session.close()
-#         except:
-#             db.session.rollback()
-#         return render_template('results.html', results=query_db,
-#                                num_return=num_return)
-#
-#     return render_template('index.html', form1=form1, form2=form2)
 
 
 @application.route('/apply', methods=['GET', 'POST'])
@@ -75,6 +43,14 @@ def apply():
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        # return redirect(url_for(''))
+        return render_template('results.html')
+    form = Login(request.form)
+    if form.validate():
+        
+
+
     form = Login(request.form)
     if request.method=='POST' and form.validate():
 
