1738715df50c43c71054eef96e15b594de8da722
Landon Smith <landjbs@gmail.com>
2020-07-10 19:06:24 -0400

    apply app

--- application.py
+++ application.py
@@ -1,11 +1,6 @@
-'''
-Simple Flask application to test deployment to Amazon Web Services
-Uses Elastic Beanstalk and RDS
-Author: Scott Rodkey - rodkeyscott@gmail.com
-Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
-'''
-
 from flask import Flask, render_template, request
+from fl
+
 from application import db
 from application.models import User
 from application.forms import Apply, Login
@@ -80,7 +75,9 @@ def apply():
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
-    form =
+    form = Login(request.form)
+    if request.method=='POST' and form.validate():
+
     return render_template('login.html')
 
 
