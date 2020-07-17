cd1debba63f5a9e0adde4d000021a59781248be3
landjbs <landjbs@gmail.com>
2020-07-16 22:03:51 -0400

    manager

---
+++ manager.py
@@ -0,0 +1,8 @@
+from application import db
+from application.models import Project, User, Member_Role, Role
+
+
+
+def create_role(title, color):
+    db.session.add(Role(title, color))
+    db.session.commit()
