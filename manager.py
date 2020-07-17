663125c11481c91acc4fb80162d2b5349e8e60b5
landjbs <landjbs@gmail.com>
2020-07-16 22:04:25 -0400

    manager

--- manager.py
+++ manager.py
@@ -2,7 +2,11 @@ from application import db
 from application.models import Project, User, Member_Role, Role
 
 
-
 def create_role(title, color):
     db.session.add(Role(title, color))
+    db.session.commit()
+
+
+def create_subject(name, color):
+    db.session.add(Subject(name, color))
     db.session.commit()
