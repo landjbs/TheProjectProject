b7f96141ec44e5e6250a4a13a89bb78248478ab5
landjbs <landjbs@gmail.com>
2020-07-16 22:08:15 -0400

    manager

--- manager.py
+++ manager.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Project, User, Member_Role, Role
 
+## CACHE ROLES ##
+creator_role = db.session.query(Role).filter_by(name=='Creator').first()
 
 def create_role(title, color):
     db.session.add(Role(title, color))
@@ -15,3 +17,10 @@ def create_subject(name, color):
 def create_user(user):
     db.session.add(user)
     db.session.commit()
+
+
+def create_project(project, user):
+    a = Member_Role(creator_role)
+    a.project = project
+    user.projects.append(a)
+    db.session.add(project)
