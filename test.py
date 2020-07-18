d4503324b65eecd9ec071e8f1807b329bb0a74ca
landjbs <landjbs@gmail.com>
2020-07-18 17:58:16 -0400

    project

--- test.py
+++ test.py
@@ -1,60 +1,11 @@
-from manager import add_user_to_project, create_project, create_role, db
-from application.models import Project, User, Role
+import requests
+from pprint import pprint
 
-user = User('Landon', 'b', '', '', '', '')
-user2 = User('2', 'l', '', '', '', '')
-project = Project('b', '', '', '', '', False, '', 19, 10, False, user)
-role = db.session.query(Role).filter_by(name='Data Scientist').first()
-
-
-create_project(project, user)
-
-for x in db.session.query(Project):
-    for m in x.members:
-        print(m.user, m.role)
-
-
-add_user_to_project(project, user2, role)
-
-for x in db.session.query(Project):
-    for m in x.members:
-        print(m.user, m.role)
-
-
-# from application import db
-# from application.models import User, Project, Subject
-
-
-# ai = Subject(name='ai')
-#
-# u = User(name='landon',
-#         email='land@',
-#         password='boop',
-#         subjects=[ai],
-#         github='github',
-#         about='Hi')
-#
-# db.session.add(ai)
-# db.session.add(u)
-
-# u2 = User('l', 'e', 'p', 'g', 'a')
-#
-# db.session.add(u)
-#
-# u = db.session.query(User).get(1)
-#
-#
-# p = Project(name='n',
-#             summary='p',
-#             url='d',
-#             creator=u,
-#             open=True,
-#             requires_application=True,
-#             application_question='b',
-#             estimated_time=3.,
-#             complete=False)
-#
-# db.session.add(u)
-
-
-# db.session.commit()
+# github username
+username = "Harrison-Oatman"
+# url to request
+url = f"https://api.github.com/users/{username}"
+# make the request and return the json
+user_data = requests.get(url).json()
+# pretty print JSON data
+pprint(user_data)
