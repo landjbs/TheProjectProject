42b64a529110d0b00a291b0b8596bc8091c3d602
landjbs <landjbs@gmail.com>
2020-07-19 14:16:48 -0400

    tasks

--- application/create_fillers.py
+++ application/create_fillers.py
@@ -175,6 +175,7 @@ def create_fillers():
         db.session.add(t)
     t = Task(text='Done this.', author=user2, project=p)
     t.mark_complete(user1)
+    t.add_worker(user2)
     db.session.add(t)
     p = db.session.query(Project).get(2)
     for i in range(10):
