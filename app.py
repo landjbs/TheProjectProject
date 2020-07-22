c38fc4e3e5bf070a2e2dd8e455484c1c1d089f26
landjbs <landjbs@gmail.com>
2020-07-22 00:00:40 -0400

    subject

--- app.py
+++ app.py
@@ -361,8 +361,12 @@ def project(project_name):
 @application.route('/subject=<subject_name>')
 def subject(subject_name):
     subject = Subject.query.filter_by(code=subject_name).first_or_404()
-    subject_projects = Project.query.filter(Project.subjects.contains(subject)).limit(30)
+    # project tabs
+    subject_projects = Project.query.filter(Project.subjects.contains(subject)).limit(300)
     subject_tabs = partition_query(subject_projects)
+    # users
+    user_tabs = []
+    subject_tabs = []
     return redirect(request.referrer)
 
 
