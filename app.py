a6849176273f801ae0a952e3122a248b8121e8c9
landjbs <landjbs@gmail.com>
2020-07-22 14:40:38 -0400

    subject

--- app.py
+++ app.py
@@ -76,10 +76,10 @@ def tasks_to_daily_activity(tasks):
     start_stamps = []
     end_stamps = []
     for task in tasks:
-        start_stamps.append(round((current_time-task.post_stamp).seconds * (1/(60)) ))
+        start_stamps.append(round((current_time-task.post_stamp).seconds * (1/(60**2)) ))
         # start_stamps.append((current_time-task.post_stamp).days)
         if task.complete:
-            end_stamps.append(round(((current_time-task.complete_stamp).seconds * (1/(60)) )))
+            end_stamps.append(round(((current_time-task.complete_stamp).seconds * (1/(60**2)) )))
             # end_stamps.append((current_time-task.complete_stamp).days)
     start_activity = Counter(start_stamps)
     end_activity = Counter(end_stamps)
