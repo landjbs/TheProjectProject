8a3cf179b0677963d6155124851e842153739329
landjbs <landjbs@gmail.com>
2020-07-21 23:32:11 -0400

    search

--- app.py
+++ app.py
@@ -499,16 +499,17 @@ def search():
     # project results
     project_results = Project.query.filter(Project.name.contains(search_text) |
                                    Project.oneliner.contains(search_text))
-    project_tabs = partition_query(project_results)
+    project_tabs = partition_query(project_results.limit(30))
     # user results
     user_results = User.query.filter(User.name.contains(search_text) |
                                      User.about.contains(search_text))
-    user_tabs = partition_query(user_results)
+    user_tabs = partition_query(user_results.limit(30))
     # subject results
     subject_results = Subject.query.filter(Subject.name.contains(search_text))
-    subject_tabs = partition_query(subject_results)
+    subject_tabs = partition_query(subject_results.limit(30))
     return render_template('search.html', project_tabs=project_tabs,
-                        user_tabs=user_tabs, subject_tabs=subject_tabs)
+                        user_tabs=user_tabs, subject_tabs=subject_tabs,
+                        search_text=search_text)
 
 
 @application.route('/logout')
