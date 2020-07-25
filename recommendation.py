4ecbcbb0b9ff765f170a4ba4a8f58c0ffa4fc8ed
landjbs <landjbs@gmail.com>
2020-07-25 15:14:03 -0400

    ranking

--- recommendation.py
+++ recommendation.py
@@ -8,11 +8,16 @@ from application.models import User, Project, Subject
 PROJECT_LIMIT = 30
 
 
+def score_project(user, project):
+    ''' Assigns project ranking '''
+
+
 def recommend_projects(user):
-    # get all open and not completed projects of which user is member
+    ## get initial candidates ##
     user_projects = [p.id for p in user.projects]
-    user_projects = Project.query.filter(Project.open==True,
-                                         Project.complete==False,
-                                         ~Project.id.in_(user_projects))
-    return user_projects
+    candidates = Project.query.filter(Project.open==True,
+                                      Project.complete==False,
+                                      ~Project.id.in_(user_projects))
+    ## score each candidate ##
+    return candidates
     # return Project.query.filter(~Project.in_(Project.query.filter(user in )))
