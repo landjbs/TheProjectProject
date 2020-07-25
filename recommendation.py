79de8f7b076a938b9fdb97f0b75ddcff4d02802a
landjbs <landjbs@gmail.com>
2020-07-24 20:18:10 -0400

    remove messaging

--- recommendation.py
+++ recommendation.py
@@ -5,5 +5,8 @@ Ranking, sorting, and searching algorithms for projects, users, and subjects.
 from application.models import User, Project, Subject
 
 
+PROJECT_LIMIT = 30
+
+
 def recommend_projects(user):
-    pass
+    Project.query.filter(Project.owner!=current_user &)
