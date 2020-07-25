7b836910da21737575312f7ffe210524eb74904d
landjbs <landjbs@gmail.com>
2020-07-24 20:05:32 -0400

    remove messaging

--- recommendation.py
+++ recommendation.py
@@ -3,3 +3,7 @@ Ranking, sorting, and searching algorithms for projects, users, and subjects.
 '''
 
 from application.models import User, Project, Subject
+
+
+def recommend_projects(user):
+    pass
