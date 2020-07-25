22e22d68dbeeec001254815aeb0d077e26a90666
landjbs <landjbs@gmail.com>
2020-07-25 15:15:20 -0400

    ranking

--- recommendation.py
+++ recommendation.py
@@ -12,12 +12,16 @@ def score_project(user, project):
     ''' Assigns project ranking '''
 
 
+
+
 def recommend_projects(user):
     ## get initial candidates ##
     user_projects = [p.id for p in user.projects]
     candidates = Project.query.filter(Project.open==True,
                                       Project.complete==False,
                                       ~Project.id.in_(user_projects))
+    ## format user preferences ##
+    
     ## score each candidate ##
     return candidates
     # return Project.query.filter(~Project.in_(Project.query.filter(user in )))
