c1d475bc64223cb90bb0b1da0625d0cf16e6f0bd
landjbs <landjbs@gmail.com>
2020-07-31 14:07:47 -0400

    mailing

--- application/tasks.py
+++ application/tasks.py
@@ -1,4 +1,5 @@
-import time
+import os
+import boto3
 import sys
 
 sys.path.append('.')
@@ -6,9 +7,17 @@ sys.path.append('.')
 from application import db
 from application.models import User, Subject
 
+SES_REGION = None
+AWS_ACCESS_KEY_ID = None
+AWS_SECRET_ACCESS_KEY= None
 
 def send_email(user_email, body):
-    time.sleep(1)
+    ses = boto3.client('ses',
+                       region_name=SES_REGION,
+                       aws_access_key_id=AWS_ACCESS_KEY_ID,
+                       secret_access_key=AWS_SECRET_ACCESS_KEY
+                    )
+    ses.send_email(Source=os.getenv('SES_EMAIL_SOURCE'), )
     user = User.query.filter_by(email=user_email).first()
     # user.emailed = True
     # db.session.commit()
