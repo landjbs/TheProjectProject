ff554b22049e86c5c625721c990bcf29ae2b3617
landjbs <landjbs@gmail.com>
2020-08-01 15:20:56 -0400

    fields

--- application/create_statics.py
+++ application/create_statics.py
@@ -5,33 +5,45 @@ from application import db
 from application.models import Subject, Badge
 
 
-subjects = [Subject('AI/ML',                    '#FF5733'),
+subjects = [# data/math/ai
+            Subject('AI/ML',                    '#FF5733'),
+            Subject('Data Science',             '#FFF033'),
+            Subject('Math',                     '#3342FF'),
+
+            # computational theory
             Subject('Algorithms',               '#FFA533'),
+            Subject('Theory',                   '#3352FF')
+            Subject('Security/Cryptography',    '#49FF33'),
+
+            # architecture/os
             Subject('Databases',                '#FFF033'),
+            Subject('Operating Systems',        '#33FFFF'),
+            Subject('Programming Languages',    '#FF33E9'),
+            Subject('Networks',                 '#33FFFF'),
+            Subject('Parallel Computing',       '#335BFF'),
+
+            # fields
             Subject('Chemistry',                '#FFF033'),
             Subject('Biology',                  '#B8FF33'),
-            Subject('Data Science',             '#FFF033'),
             Subject('Engineering',              '#00B6FF'),
             Subject('Finance',                  '#00B6FF'),
-            Subject('Graphics/Design',          '#58FF33'),
             Subject('Gaming',                   '#FF00B9'),
+            Subject('Physics',                  '#B8FF33'),
+
+            # art
+            Subject('Graphics/Design',          '#58FF33'),
             Subject('Hacking',                  '#33FFCA'),
             Subject('Hardware',                 '#FF3333'),
-            Subject('Math',                     '#3342FF'),
             Subject('Music',                    '#3342FF'),
+
+            # dev/engineering
             Subject('Mobile Dev',               '#E333FF'),
-            Subject('Networking',               '#33FFFF'),
-            Subject('Operating Systems',        '#33FFFF'),
-            Subject('Parallel Computing',       '#335BFF'),
             Subject('Web Dev',                  '#8D33FF'),
-            Subject('Programming Languages',    '#FF33E9'),
-            Subject('Physics',                  '#B8FF33'),
             Subject('Research',                 '#DDFF33'),
-            Subject('Security/Cryptography',    '#49FF33'),
             Subject('Software Engineering',     '#AF33FF'),
             Subject('Social Issues',            '#33DAFF'),
             Subject('Startup',                  '#33C1FF'),
-            Subject('Theory',                   '#3352FF')]
+            ]
 
 
 # badges = [Badge('Super-Owner', ),    # own and complete many projects
