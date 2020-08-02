2d6735adaea84e6a46bb41575160a436c1c43820
landjbs <landjbs@gmail.com>
2020-08-01 21:24:29 -0400

    admin

--- auth.py
+++ auth.py
@@ -14,9 +14,7 @@ from flask import (Flask, render_template, request, flash, redirect,
 ## views ##
 class AdminBaseView(ModelView):
     def is_accessible(self):
-        return (current_user.is_active and
-                current_user.is_authenticated and
-                current_user.is_admin())
+        return (current_user.is_admin())
 
     def _handle_view(self, name, **kwargs):
         if not self.is_accessible():
