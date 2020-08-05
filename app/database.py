cc07534022b57d11b3846a639da29c6cc3fa526a
landjbs <landjbs@gmail.com>
2020-08-05 19:09:24 -0400

    user set password

--- app/database.py
+++ app/database.py
@@ -36,3 +36,13 @@ class CRUDMixin(object):
     def delete(self, commit=True):
         db.session.delete(self)
         return commit and db.session.commit()
+
+
+
+def generate_code(name, table):
+    ''' Generate unique code for url to access name in table '''
+    code = str(name).replace('/', '_').replace(' ', '_').lower()
+    temp_code = f'{code}_{str(np.random.randint(0, 1000))}'
+    while table.query.filter_by(code=temp_code).first() is not None:
+        temp_code = f'{code}_{str(np.random.randint(0, 1000))}'
+    return temp_code
