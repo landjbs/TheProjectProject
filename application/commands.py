e059441e41f3e7050c15bd5122effcbd8798e91f
landjbs <landjbs@gmail.com>
2020-08-04 23:39:34 -0400

    commands

---
+++ application/commands.py
@@ -0,0 +1,42 @@
+import click
+from faker import Faker
+
+
+from app.database import db
+
+
+
+@click.option('--num_users', default=5, help='Number of users.')
+def populate_db(num_users):
+    ''' Populates db with seed '''
+    fake = Faker()
+    users = []
+    # fake users
+    for _ in range(num_users):
+        users.append(
+            User(
+                name=fake.name(),
+                email=fake.email(),
+                password=(fake.word()+fake.word()),
+                github=f'https://github.com/{fake.name()}',
+                about=' '.join([fake.word() for _ in range(30)])
+            )
+        )
+
+
+def create_db():
+    ''' Creates database '''
+    db.create_all()
+    return True
+
+
+def drop_db():
+    if click.confirm('Are you sure you want to drop the database?', abort=True):
+        db.drop_all()
+    return True
+
+
+def rebuild_db():
+    ''' Drops database and then creates '''
+    drop_db()
+    create_db()
