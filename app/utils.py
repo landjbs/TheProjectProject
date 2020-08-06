e38edbb0b1a5b1202ace75c579006b73b9e5b099
landjbs <landjbs@gmail.com>
2020-08-06 18:22:03 -0400

    user page

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,30 @@
 from flask import request, url_for
 
 
+def tasks_to_daily_activity(tasks):
+    current_time = datetime.utcnow()
+    start_stamps = []
+    end_stamps = []
+    for task in tasks:
+        start_stamps.append(round((current_time-task.post_stamp).days))
+        # start_stamps.append((current_time-task.post_stamp).days)
+        if task.complete:
+            end_stamps.append(round(((current_time-task.complete_stamp).days)))
+            # end_stamps.append((current_time-task.complete_stamp).days)
+    start_activity = Counter(start_stamps)
+    end_activity = Counter(end_stamps)
+    earliest = max(start_activity)+1
+    for i in range(earliest):
+        if i not in start_activity:
+            start_activity.update({i:0})
+        if i not in end_activity:
+            end_activity.update({i:0})
+    start_activity = [x[1] for x in sorted(start_activity.items(), key=itemgetter(0), reverse=True)]
+    end_activity = [x[1] for x in sorted(end_activity.items(), key=itemgetter(0), reverse=True)]
+    # for i in range(start_activity):
+    return (start_activity, end_activity, earliest)
+
+
 def url_for_other_page(**kwargs):
     ''' Generates target url with pjax and url_for_args '''
     url_for_args = request.arg.copy()
