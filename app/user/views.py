6b85e183d92938dc1d898f9947576f5a0eb578a9
landjbs <landjbs@gmail.com>
2020-08-06 18:21:16 -0400

    user page

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,9 @@
 from flask import request, redirect, url_for, render_template, flash, g
 from flask_babel import gettext
 from flask_login import login_required
+
+
+from app.extensions import limiter
 from app.user.models import User
 # from .forms import EditUserForm
 
