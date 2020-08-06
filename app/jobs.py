32af76abbc9b01ba8a324a08cadd2c4540c789b1
landjbs <landjbs@gmail.com>
2020-08-06 14:32:47 -0400

    global css

--- app/jobs.py
+++ app/jobs.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import sys
+from app.extensions import rq
 
 sys.path.append('.')
 
@@ -13,13 +14,13 @@ SES_EMAIL_SOURCE = 'admin@theprojectproject.io'
 AWS_ACCESS_KEY_ID = 'AKIAQVSLC6YU44B3L5FB'
 AWS_SECRET_ACCESS_KEY= '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'
 
+ses = boto3.client('ses',
+                   region_name=SES_REGION,
+                   aws_access_key_id=AWS_ACCESS_KEY_ID,
+                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+                )
 
 def send_email(user_email, body):
-    ses = boto3.client('ses',
-                       region_name=SES_REGION,
-                       aws_access_key_id=AWS_ACCESS_KEY_ID,
-                       aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-                    )
     ses.send_email(Source=SES_EMAIL_SOURCE,
                    Destination={'ToAddresses':[user_email]},
                    Message= {'Subject':{'Data': 'Confirm Your Account'},
@@ -28,3 +29,19 @@ def send_email(user_email, body):
     user.emailed = True
     db.session.commit()
     db.session.close()
+
+
+@rq.job
+def send_registration_email(user_id, token):
+    user = User.query.filter_by(id=user_id).first()
+    ses.send_email(
+        Source=SES_EMAIL_SOURCE,
+        Destination={'ToAddresses': [user.email]},
+        Message={
+            'Subject': {'Data': 'Confirm Your Account'},
+            'Body': {
+                'Html': {'Data': render_template('mail/register.mail',
+                                                 user=user, token=token)}
+            }
+        }
+    )
