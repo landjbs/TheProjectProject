e6b43e31e14709388f77818c2a193126cd060eee
landjbs <landjbs@gmail.com>
2020-08-06 19:37:18 -0400

    elasticsearch funcs

--- app/recommendations/search.py
+++ app/recommendations/search.py
@@ -1,1 +1,10 @@
 from flask import current_app
+
+
+def add_to_index(index, model):
+    if not current_app.elasticsearch:
+        return None
+    payload = {}
+    for field in model.__searchable__:
+        payload[field] = getattr(model, field)
+    current_app.elasticsearch.index(index=index, id=model.id, body=payload)
