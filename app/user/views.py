cb54fd9e24d72f896555d23ea2d3af11788e6cd3
landjbs <landjbs@gmail.com>
2020-08-07 16:53:05 -0400

    collab

--- app/user/views.py
+++ app/user/views.py
@@ -143,10 +143,11 @@ def collaborate(target_user_id):
 def accept_collaboration(project_id):
     project = Project.query.get_or_404(project_id)
     if current_user in project.invitations:
-        flash(f'You have accepted the invitation to {project.name}.')
-        manager.add_user_to_project(current_user, project)
+        flash(f'You have accepted the invitation to {project.name}.', 'success')
+        project.add_member(current_user)
     else:
-        flash(f'Could not join {current_user.name} as you have not been invited.')
+        flash(f'Could not join {current_user.name} as you have not been invited.',
+               'error')
     return redirect(request.referrer)
 
 
