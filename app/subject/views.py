b4fa2aed7e28ec2d65cd529cd030ee49b9e2e9c8
landjbs <landjbs@gmail.com>
2020-08-07 20:36:40 -0400

    create subject

--- app/subject/views.py
+++ app/subject/views.py
@@ -15,15 +15,19 @@ from ..subject import subject
 def subject(subject_name):
     subject = Subject.query.filter_by(code=subject_name).first_or_404()
     # project tabs
-    subject_projects = Project.query.filter(Project.subjects.contains(subject)).limit(30)
-    project_tabs = list(partition_query(subject_projects))
+    subject_projects = Project.query.filter(Project.subjects.contains(subject))
+    project_count = subject_projects.count()
+    project_tabs = list(partition_query(subject_projects.limit(30)))
     # users
+    user_count = len(subject.users)
     subject_users = [s.user for s in subject.users[:30]]
     user_tabs = partition_query(subject_users)
     # application
     project_application = Project_Application_Form()
     return render_template('subject.html',
                            project_tabs=project_tabs,
+                           project_count=project_count,
                            user_tabs=user_tabs,
-                           subject_name=subject.name,
+                           user_count=user_count,
+                           subject=subject,
                            project_application=project_application)
