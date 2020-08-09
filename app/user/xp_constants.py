6742413b78934ccadfd2d6248c09ccf1d1d0ab45
landjbs <landjbs@gmail.com>
2020-08-09 14:25:44 -0400

    badges

--- app/user/xp_constants.py
+++ app/user/xp_constants.py
@@ -6,9 +6,19 @@ Constants defining xp allocated for each action
 class XP_Constants(object):
     def __init__(self):
         self.xps = {
-            'star_project'  :   1,
-            'earn_star'      :   10,
-
+            ## stars ##
+            'star_project'      :   1,
+            'earn_star'         :   50,
+            ## collab ##
+            'send_collab'       :   5,
+            'recieve_collab'    :   10,
+            ## tasks ##
+            'post_task'         :   30,
+            'complete_task'     :   50,
+            ## projects ##
+            'complete_project'  :   500,
+            'join_project'      :   100,
+            # 'own_project'       :   ,
         }
 
     def action_xp(self, action:str):
