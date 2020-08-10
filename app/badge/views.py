668be06a5548e42466f0fcc89e6444b229343c59
landjbs <landjbs@gmail.com>
2020-08-10 10:12:07 -0400

    badge dispalyg

--- app/badge/views.py
+++ app/badge/views.py
@@ -1,8 +1,9 @@
 from flask import request, redirect, url_for, render_template, flash
 from flask_login import login_required, current_user
 
-from .models import Badge
+from app.utils import partition_query
 
+from .models import Badge
 from ..badge import badge
 
 
@@ -12,6 +13,8 @@ from ..badge import badge
 def badge_page():
     # update badges
     current_user.update_badges()
+    # progress tabs
+    progress_tabs = partition_query(current_user.badges)
     # all badges
     all_badges = Badge.query.all()
     return render_template('badge.html', all_badges)
