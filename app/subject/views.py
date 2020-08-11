f4845926cd42d0ee9c52bef23082d1b843c00e29
landjbs <landjbs@gmail.com>
2020-08-10 20:25:43 -0400

    mobile search

--- app/subject/views.py
+++ app/subject/views.py
@@ -1,5 +1,6 @@
 from flask import request, redirect, url_for, render_template, flash
 from flask_login import login_required, current_user
+from flask_mobility.decorators import mobilized
 # absolute imports
 from app.utils import partition_query
 from app.project.models import Project
@@ -10,8 +11,6 @@ from .models import Subject
 from ..subject import subject
 
 
-@subject.route('/subject=<subject_name>')
-@login_required
 def subject(subject_name):
     subject = Subject.query.filter_by(code=subject_name).first_or_404()
     # project tabs
@@ -31,3 +30,26 @@ def subject(subject_name):
                            user_count=user_count,
                            subject=subject,
                            project_application=project_application)
+
+
+@subject.route('/subject=<subject_name>')
+@login_required
+@mobilized(subject)
+def subject(subject_name):
+    ''' Mobile optimized route for subject page '''
+    subject = Subject.query.filter_by(code=subject_name).first_or_404()
+    # projects
+    subject_projects = Project.query.filter(Project.subjects.contains(subject))
+    project_count = subject_projects.count()
+    # users
+    user_count = len(subject.users)
+    subject_users = [s.user for s in subject.users[:30]]
+    # format result data
+    results = {'project'    :   (subject_projects, project_count),
+               'user'       :   (subject_users, user_count)}
+    # form
+    project_application = Project_Application_Form()
+    return render_template('subject_mobile.html',
+                        results=results,
+                        subject=subject,
+                        user_results=subject_users)
