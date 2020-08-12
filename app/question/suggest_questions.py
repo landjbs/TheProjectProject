629b3b8ac8e1f2102137c1249400fb8f85e31c8f
landjbs <landjbs@gmail.com>
2020-08-12 13:16:48 -0400

    questions

--- app/question/suggest_questions.py
+++ app/question/suggest_questions.py
@@ -7,6 +7,9 @@ questions but that's way later.
 question_suggestions = [
     ########### INSPIRATION: How you came up with idea #########################
     'How did you come up with this idea?',
-    ''
+    ############################################################################
+    ############## SO FAR: How the process is going ############################
+    'What is the most difficult challenge you have faced?',
+    'What have you learned through your work on this project?',
     ############################################################################
 ]
