98dc4b264b4ae607551ebdb7b888089955581bc5
landjbs <landjbs@gmail.com>
2020-08-12 13:52:53 -0400

    questions

--- app/question/models.py
+++ app/question/models.py
@@ -13,8 +13,10 @@ class Question(CRUDMixin, db.Model):
     project = relationship('Project', back_populates='questions')
     # question
     question = db.Column(db.String(100), nullable=False, unique=False)
+    asked_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     # answer
     answer = db.Column(db.String(500), nullable=True, unique=False)
+    answered_on = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
         return(f'<Question_Answer q={self.question} a={self.answer} '
