e0412f322babfc335f8b1bdf16f0ca4de4944cb3
landjbs <landjbs@gmail.com>
2020-08-11 21:00:42 -0400

    about

--- app/questions/models.py
+++ app/questions/models.py
@@ -8,7 +8,13 @@ class Question(CRUDMixin, db):
     __tablename__ = 'question'
     # id
     id = db.Column(db.Integer, primary_key=True)
-    # question
-    question = db.Column(db.String(100), nullable=True)
+    # text
+    text = db.Column(db.String(100), nullable=True, unique=True)
+
+
+class Answer(CRUDMixin, db):
+    __tablename__ = 'answer'
+    # id
+    id = db.Column(db.Integer, primary_key=True)
     # answer
     answer = db.Column(db.String(500), nullable=True)
