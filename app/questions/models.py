36e66153e607eecbc3d720ee22a09aee82d84bc1
landjbs <landjbs@gmail.com>
2020-08-12 11:01:49 -0400

    about

--- app/questions/models.py
+++ app/questions/models.py
@@ -11,7 +11,7 @@ class Question(CRUDMixin, db):
     # question
     question = db.Column(db.String(100), nullable=True, unique=True)
     # answers
-    answers = relationship('Question_Answer', )
+    answers = relationship('Question_Answer', back_populates='question')
 
     def __repr__(self):
         return f'<Question {self.question}>'
@@ -21,7 +21,9 @@ class Question_Answer(CRUDMixin, db):
     __tablename__ = 'answer'
     # id
     id = db.Column(db.Integer, primary_key=True)
-    #
+    # question
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+    question = relationship('Question', back_populates='answers')
     # answer
     answer = db.Column(db.String(500), nullable=True)
 
