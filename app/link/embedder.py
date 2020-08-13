f84b4785ccdb080cc9ab6fada4bfd429734ea2ea
landjbs <landjbs@gmail.com>
2020-08-13 17:55:12 -0400

    link

--- app/link/embedder.py
+++ app/link/embedder.py
@@ -1,2 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+def get_description(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text)
+    metas = soup.find_all('meta')
+    return [meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ]
