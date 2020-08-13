4b16d3a6ecf4d5cb8932d1f2b3bd72c29ca52670
landjbs <landjbs@gmail.com>
2020-08-13 18:35:31 -0400

    link

--- app/link/embedder.py
+++ app/link/embedder.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_description(url):
+def scrape(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text)
     metas = soup.find_all('meta')
     description = [meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ]
-    title = soup.find('title').string
-    return {'title':title, 'description':description}
+    description = description[0] if len(description)>0 else None
+    return {'description':description}
--- app/link/models.py
+++ app/link/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy.orm import relationship
 
-import .embedder
+from .embedder import scrape
 
 from app.database import db, CRUDMixin
 
@@ -16,16 +16,14 @@ class Link(CRUDMixin, db.Model):
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=True)
     project = relationship('Project', back_populates='links')
     ## render data (potentially should be moved somewhere more efficient) ##
-    title = db.Column(db.String(500), nullable=True, unique=False)
-    description = db.Column(db.Text(), nullable=True, unique=False)
+    description = db.Column(db.Text(100000), nullable=True, unique=False)
     is_rendered = db.Column(db.Boolean, nullable=False, default=False)
 
     def __init__(self, url):
         self.url = url
-        scape_data = embedder.scrape(url)
+        scape_data = scrape(url)
         ## load page data ##
-        self.title = scape_data['title']
-        self.description = scape_data['description']
+        self.description = str(scape_data['description'])
         self.is_rendered = True
 
     def __repr__(self):
