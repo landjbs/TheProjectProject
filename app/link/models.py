43b2abab9f2d55502e284fa1452d79e6e103d0e4
landjbs <landjbs@gmail.com>
2020-08-14 11:48:11 -0400

    private links

--- app/link/models.py
+++ app/link/models.py
@@ -16,17 +16,17 @@ class Link(CRUDMixin, db.Model):
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=True)
     project = relationship('Project', back_populates='links')
     # is link public
-    public = db.Column(db.Boolean, nullable=False, default=True)
+    # public = db.Column(db.Boolean, nullable=False, default=True)
     # category within project: {1:repo, 2:communication, 3:drive: 4:management, 5:other}
-    category = db.Column(db.Integer, nullable=False, default=0)
+    # category = db.Column(db.Integer, nullable=False, default=0)
     ## render data (potentially should be moved somewhere more efficient) ##
     description = db.Column(db.Text(100000), nullable=True, unique=False)
     is_rendered = db.Column(db.Boolean, nullable=False, default=False)
 
     def __init__(self, url, public, category):
         self.url = url
-        self.public = public
-        self.category = category
+        # self.public = public
+        # self.category = category
         scape_data = scrape(url)
         ## load page data ##
         self.description = str(scape_data['description'])
