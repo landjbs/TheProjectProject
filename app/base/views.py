bc6e13a2e016fcb4c08f97e86f6bfcc29228b7f4
landjbs <landjbs@gmail.com>
2020-08-14 16:11:47 -0400

    add instructions

--- app/base/views.py
+++ app/base/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, send_from_directory, current_app, request
 
 from ..base import base
 
@@ -21,3 +21,9 @@ def contact():
 @base.route('/terms', methods=['GET'])
 def terms():
     return render_template('terms.html')
+
+
+@base.route('/robots.txt')
+@base.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(current_app.static_folder, request.path[1:])
