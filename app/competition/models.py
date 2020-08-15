b5a1e7a961b7fa7d1edd1a6428fbbd8aba6e0654
landjbs <landjbs@gmail.com>
2020-08-15 13:56:23 -0400

    share temp

--- app/competition/models.py
+++ app/competition/models.py
@@ -26,7 +26,7 @@ class Competition(CRUDMixin, db.Model):
     winners = relationship('Project',
                         secondary='hackathon_to_project')
     ## administrative ##
-    active = db.Column(db.Boolean, nullable=False)
+    active = db.Column(db.Boolean, nullable=False, default=True)
     complete = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
