e31e29c2f8a5be1d34fbc2e821e9f71ad1d896b2
landjbs <landjbs@gmail.com>
2020-08-14 21:41:09 -0400

    hackathon

--- app/hackathon/models.py
+++ app/hackathon/models.py
@@ -9,6 +9,8 @@ class Hackathon(CRUDMixin, db.Model):
     __tablename__ = ''
     # id
     id = db.Column(db.Integer, primary_key=True)
+    # name
+    name = db.Column(db.String(100), nullable=False)
     # sponsor
     sponsor = db.Column(db.String(100), nullable=True)
     # description
@@ -18,3 +20,12 @@ class Hackathon(CRUDMixin, db.Model):
     # timing
     starts_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     ends_on = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<Hackathon {self.name} by {self.sponsor}>'
+
+    def time_progressed(self):
+        return (datetime.utcnow() - self.starts_on).days
+
+    def time_remaining(self):
+        return (self.ends_on - datetime.utcnow()).days
