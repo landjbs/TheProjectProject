283b4e9abe935b40ae6a3b4c948bf7c2c1a871a9
landjbs <landjbs@gmail.com>
2020-08-16 12:50:18 -0400

    search

--- app/hub/forms.py
+++ app/hub/forms.py
@@ -4,3 +4,14 @@ from wtforms.validators import (DataRequired, Length, EqualTo, Email)
 from app.user.models import User
 from app.forms.base import BaseForm
 from app.forms.validators import Site_URL_Validator, Select_Limit_Validator
+
+
+class Search_Form(BaseForm):
+    search = StringField('Search', validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(Search_Form, self).__init__(*args, **kwargs)
