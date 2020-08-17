94638ffa5e85702b64722e17c75ca4977577693d
landjbs <landjbs@gmail.com>
2020-08-16 20:23:27 -0400

    config

--- app/config.py
+++ app/config.py
@@ -7,23 +7,19 @@ class base_config(object):
     def __init__(self):
         # SITE
         SITE_NAME = os.environ.get('APP_NAME', 'TheProjectProject')
-
         # SERVER
         SECRET_KEY = os.environ.get('SECRET_KEY', 'secrets')
-
         # MAIL
         SES_REGION = 'us-east-1'
         SES_EMAIL_SOURCE = 'admin@theprojectproject.io'
         AWS_ACCESS_KEY_ID = 'AKIAQVSLC6YU44B3L5FB'
         AWS_SECRET_ACCESS_KEY= '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'
-
-
         # SQLALCHEMY URI
-        db_user = 'admin'
-        db_password = 'jl245o234jDFalsdkjf;kl2j4508usdjilfka'
-        endpoint = 'theprojectproject.c4u7frshhdtj.us-east-1.rds.amazonaws.com:3306'
-        db_url = 'dev_db'
-
+        # uri = f'mysql+pymysql://admin:sk90jal;skdjn,235#adsfjalasdf#%n2sdf@theprojectproject.c4u7frshhdtj.us-east-1.rds.amazonaws.com:3306/theprojectproject_production'
+        # db_user = 'admin'
+        # db_password = 'jl245o234jDFalsdkjf;kl2j4508usdjilfka'
+        # endpoint = 'theprojectproject.c4u7frshhdtj.us-east-1.rds.amazonaws.com:3306'
+        # db_url = 'dev_db'
         DB_USER = os.environ.get('DB_USER', 'admin')
         DB_PASSWORD = os.environ.get('DB_PASSWORD')
         DB_ENDPOINT = os.environ.get('DB_ENDPOINT')
@@ -35,23 +31,30 @@ class base_config(object):
             DB_ENDPOINT,
             DB_URL
         )
-
         # SQLALCHEMY SETTINGS
-        self.SQLALCHEMY_POOL_RECYCLE = 3600
+        SQLALCHEMY_POOL_RECYCLE = 3600
         SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-        self.SEND_FILE_MAX_AGE_DEFAULT = 0
-
         # REDIS
         self.REDIS_URL = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
         self.QUEUES = ['default']
-
         # ADMIN
         self.FLASK_ADMIN_SWITCH = 'orange'
-
         # FORMS
         self.WTF_CSRF_ENABLED = True
 
 
-
         self.SECRET_KEY = '1v8GEdD0oUBA55MQRkD/D/wS7CGmmSHyatMm0arx'
+
+
+class dev_config(base_config):
+    # SQLALCHEMY URI
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    # statics
+    SEND_FILE_MAX_AGE_DEFAULT = 0
+    # forms
+    WTF_CSRF_ENABLED = False
+    # debugging
+    ASSETS_DEBUG = True
+
+
+class production_config(base_config):
