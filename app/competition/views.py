24d8e9db6a128f52deed585f708d344dd341620f
landjbs <landjbs@gmail.com>
2020-08-17 17:44:51 -0400

    secrets

--- app/competition/views.py
+++ app/competition/views.py
@@ -1,3 +1,13 @@
+from flask import request, redirect, url_for, render_template, flash
+from flask_login import login_required, current_user
+
 from .models import Competition
 
 from ..competition import competition
+
+
+@competition.route(f'/competition=<code>', methods=['GET'])
+@login_required
+def competition_page(code):
+    competition = Competition.query.get_or_404(code)
+    return render_template('competition.html', competition=competition)
