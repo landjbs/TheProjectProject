d2b1f8f97d6b5685f743a1538602bafabebf95da
landjbs <landjbs@gmail.com>
2020-08-17 20:49:12 -0400

    main box

--- app/badge/views.py
+++ app/badge/views.py
@@ -13,7 +13,7 @@ from ..badge import badge
 
 def badge_page():
     # active competitions
-    active_competitions = Competition.get_active_competitions()
+    competitions = Competition.get_active_competitions()
     # update badges
     current_user.update_badges()
     # progress tabs
@@ -21,24 +21,6 @@ def badge_page():
     # all badges
     all_tabs = list(partition_query(Badge.query.all()))
     return render_template('badge.html',
-                            active_competitions=active_competitions,
+                            competitions=competitions,
                             progress_tabs=progress_tabs,
                             all_tabs=all_tabs)
-
-
-@badge.route('/badge', methods=['GET', 'POST'])
-@limiter.limit('60 per minute')
-@mobilized(badge_page)
-def badge_page():
-    ''' Mobile optimized badge page '''
-    # update badges
-    current_user.update_badges()
-    # progress tabs
-    progress_badges = list(current_user.badges)
-    progress_ids = [u.badge.id for u in progress_badges]
-    # all badges
-    other_badges = list(Badge.query.filter(~Badge.id.in_(progress_ids)))
-    del progress_ids
-    return render_template('badge_mobile.html',
-                           progress_badges=progress_badges,
-                           other_badges=other_badges)
