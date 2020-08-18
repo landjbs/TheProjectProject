aaf5d609a2b7835851f8fa403270d1f8cd67d543
landjbs <landjbs@gmail.com>
2020-08-18 15:03:03 -0400

    embedly rm

--- app/link/embedder.py
+++ app/link/embedder.py
@@ -1,8 +1,18 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
-## matcher for valid url ##
+## validate/fix url ##
+# matcher for valid route protocol
+url_string = r'https://\S+|http://\S+'
+url_matcher = re.compile(url_string)
 
+def parsable(url):
+    ''' Deterimines if url has valid protocol '''
+    return True if url_matcher.fullmatch(url) else False
+
+
+####################################
 
 
 def scrape(url):
