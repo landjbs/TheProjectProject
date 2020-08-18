da15170234c04cc196c09ae11dacf39e044e16c6
landjbs <landjbs@gmail.com>
2020-08-18 15:06:42 -0400

    embedly rm

--- app/link/embedder.py
+++ app/link/embedder.py
@@ -7,11 +7,20 @@ from bs4 import BeautifulSoup
 url_string = r'https://\S+|http://\S+'
 url_matcher = re.compile(url_string)
 
-def parsable(url):
+def parsable(url:str):
     ''' Deterimines if url has valid protocol '''
     return True if url_matcher.fullmatch(url) else False
 
-
+def fix_url(url:str, root:str):
+    ''' Adds proper route to url '''
+    if not parsable(url):
+        if url.startswith('http'):
+            pass
+        elif url.startswith('www'):
+            url = f'https://{url}'
+        else:
+            return False
+    return url
 ####################################
 
 
