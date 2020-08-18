59193884c6f4a1924032fa9267b0fa168d48946b
landjbs <landjbs@gmail.com>
2020-08-18 13:00:06 -0400

    fake competitions

--- app/competition/views.py
+++ app/competition/views.py
@@ -12,4 +12,5 @@ from ..competition import competition
 @login_required
 def competition_page(code):
     competition = Competition.query.filter_by(code=code).first_or_404()
+    submissions = [s.project for s in Competition.submissions]
     return render_template('competition.html', competition=competition)
