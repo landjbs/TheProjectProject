2b5f1ebfff36dae0db2e2750b32d8170de39e1f7
landjbs <landjbs@gmail.com>
2020-08-18 14:03:43 -0400

    subject fix

--- app/subject/views.py
+++ app/subject/views.py
@@ -13,23 +13,14 @@ from ..subject import subject
 
 def subject_page(subject_name):
     subject = Subject.query.filter_by(code=subject_name).first_or_404()
-    # project tabs
-    subject_projects = Project.query.filter(Project.subjects.contains(subject))
-    project_count = subject_projects.count()
-    project_tabs = list(partition_query(subject_projects.limit(30)))
+    # projects
+    projects = Project.query.filter(Project.subjects.contains(subject)).all()
     # users
-    user_count = len(subject.users)
-    subject_users = [s.user for s in subject.users[:30]]
-    user_tabs = partition_query(subject_users)
-    # application
-    project_application = Project_Application_Form()
+    users = [s.user for s in subject.users[:30]]
     return render_template('subject.html',
-                           project_tabs=project_tabs,
-                           project_count=project_count,
-                           user_tabs=user_tabs,
-                           user_count=user_count,
-                           subject=subject,
-                           project_application=project_application)
+                           projects=projects,
+                           users=users,
+                           subject=subject)
 
 
 @subject.route('/subject=<subject_name>')
