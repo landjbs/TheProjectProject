febea381cb0cfe9fc3982d0d18ad0ca982c48fa0
landjbs <landjbs@gmail.com>
2020-08-18 21:52:52 -0400

    badge fix

--- app/static/js/star.js
+++ app/static/js/star.js
@@ -1,5 +1,21 @@
 <!-- href="{{ url_for('project.like_action', project_id=project.id, action='unlike') }}" -->
 
-function toggleStar() {
-  
+function toggleStar(project_id) {
+  {% if project_id in g.current_user.stars %}
+    url = "{{ url_for('project.like_action', project_id=project.id, action='unlike') }}",
+    star = false
+  {% else %}
+    url = "{{ url_for('project.like_action', project_id=project.id, action='like') }}",
+    star = true
+  {% endif %}
+  alert(url);
+  $.ajax({
+    type: 'GET',
+    url: url,
+  })
+  if star {
+    $('#star_{{ project_id }}').class = 'fa fa-star'
+  } else {
+    $('#star_{{ project_id }}').class = 'fa fa-star-o'
+  }
 }
