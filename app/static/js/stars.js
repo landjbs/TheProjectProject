3570ca62584ac81016f6bf3a799fa430c580cea4
landjbs <landjbs@gmail.com>
2020-08-18 22:35:27 -0400

    badge fixg

--- app/static/js/stars.js
+++ app/static/js/stars.js
@@ -1,15 +1,12 @@
 function toggleStar(project_id, url, action, star_id) {
+  alert(url);
   $.ajax({
     type: 'GET',
     url: url
   })
   if (action==0) {
     document.getElementById(star_id).className = 'fa fa-star-o';
-    // document.getElementById(star_id).classList.remove('fa-star');
-    // document.getElementById(star_id).classList.add('fa-star-o');
   } else {
       document.getElementById(star_id).className = 'fa fa-star';
-    // document.getElementById(star_id).classList.remove('fa-star-o');
-    // document.getElementById(star_id).classList.add('fa-star');
   }
 }
