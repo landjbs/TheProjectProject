6541c7290738ee3ff562672d92acb5d1447f92c3
landjbs <landjbs@gmail.com>
2020-08-18 22:19:05 -0400

    badge fix

--- app/static/js/stars.js
+++ app/static/js/stars.js
@@ -1,11 +1,13 @@
-function toggleStar(project_id, url, action) {
+function toggleStar(project_id, url, action, star_id) {
   $.ajax({
     type: 'GET',
     url: url
   })
   if (action==0) {
-    $('#star_{{ project_id }}').class = 'fa fa-star'
+    $(star_id).classList.remove('fa-star');
+    $(star_id).classList.add('fa-star-o');
   } else {
-    $('#star_{{ project_id }}').class = 'fa fa-star-o'
+    $(star_id).classList.remove('fa-star-o');
+    $(star_id).classList.add('fa-star');
   }
 }
