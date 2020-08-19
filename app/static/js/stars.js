c1950471601223fdd268e2436b4766f0f47249e3
landjbs <landjbs@gmail.com>
2020-08-18 22:50:07 -0400

    starcount

--- app/static/js/stars.js
+++ app/static/js/stars.js
@@ -1,4 +1,4 @@
-function toggleStar(project_id, url, star_id) {
+function toggleStar(project_id, url, star_id, count_id) {
   $.ajax({
     type: 'GET',
     url: url,
@@ -6,6 +6,7 @@ function toggleStar(project_id, url, star_id) {
     success: function(data){
       var stars = data['stars']
       var is_starred = data['is_starred']
+      document.getElementById(count_id).innerHTML = stars + '<i class="fa fa-star"></i>';
       if (is_starred==0) {
         document.getElementById(star_id).className = 'fa fa-star-o';
       } else {
