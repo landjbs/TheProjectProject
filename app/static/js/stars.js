a2eee0ee7d2d764141a41d9db152e38bce042b14
landjbs <landjbs@gmail.com>
2020-08-18 22:08:18 -0400

    badge fix

--- app/static/js/stars.js
+++ app/static/js/stars.js
@@ -1,5 +1,5 @@
 function toggleStar(project_id, action) {
-  if (action=='star') {
+  if (action==1) {
     url = "{{ url_for('project.like_action', project_id=project.id, action='like') }}";
   } else {
     url = "{{ url_for('project.like_action', project_id=project.id, action='unlike') }}";
@@ -9,7 +9,7 @@ function toggleStar(project_id, action) {
     type: 'GET',
     url: url,
   })
-  if (action=='star') {
+  if (action==0) {
     $('#star_{{ project_id }}').class = 'fa fa-star'
   } else {
     $('#star_{{ project_id }}').class = 'fa fa-star-o'
