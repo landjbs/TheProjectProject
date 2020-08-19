2044274fe414a364358150d82fe622bee48c3e68
landjbs <landjbs@gmail.com>
2020-08-18 22:58:50 -0400

    starcount

--- app/static/js/stars.js
+++ app/static/js/stars.js
@@ -6,7 +6,7 @@ function toggleStar(project_id, url, star_id, count_id) {
     success: function(data){
       var stars = data['stars']
       var is_starred = data['is_starred']
-      document.getElementById(count_id).innerHTML = stars + '<i class="fa fa-star"></i>';
+      document.getElementById(count_id).innerHTML = stars + ' <i class="fa fa-star"></i>';
       if (is_starred==0) {
         document.getElementById(star_id).className = 'fa fa-star-o';
       } else {
