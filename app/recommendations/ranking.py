7f6940c2a0517c9823da17116a8cf79b4b926aa1
landjbs <landjbs@gmail.com>
2020-08-19 20:34:28 -0400

    form fix

--- app/recommendations/ranking.py
+++ app/recommendations/ranking.py
@@ -3,6 +3,5 @@ Ranking alorithms for sorting recommendations
 '''
 
 
-def score_and_rank_candidates(candidates, score_fn, n):
-    ''' Uses Boogle ranking algorithm to get top n scored candidates '''
-    
+# def score_and_rank_candidates(candidates, score_fn, n):
+    # ''' Uses Boogle ranking algorithm to get top n scored candidates '''
