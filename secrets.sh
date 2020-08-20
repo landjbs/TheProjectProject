b43b1fa11762037f54e123e084684e19b96ec42c
landjbs <landjbs@gmail.com>
2020-08-19 21:50:07 -0400

    session fix

--- secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export SES_REGION=us-east-1
-export SES_EMAIL_SOURCE=admin@theprojectproject.io
-export AWS_ACCESS_KEY_ID=AKIAQVSLC6YU44B3L5FB
-export AWS_SECRET_ACCESS_KEY=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
-export DB_USER = admin
-export DB_PASSWORD=fbb5f9ba9feb4326a8b9e7a0e3b3aa29
-export DB_ENDPOINT=theprojectproject.c4u7frshhdtj.us-east-1.rds.amazonaws.com:3306
-export DB_URL=theprojectproject_production
