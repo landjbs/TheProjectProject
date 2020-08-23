63c19b2c228c97a9ab796e478852d23955b72785
landjbs <landjbs@gmail.com>
2020-08-22 21:35:11 -0400

    analytics

--- app/analytics/views.py
+++ app/analytics/views.py
@@ -1,3 +1,25 @@
+from flask import Flask, Response, abort, request
+from base64 import b64decode
+import datetime
+import json
+import os
+from urlparse import parse_qsl, urlparse
+from peewee import *
+
+
+# 1 pixel GIF, base64-encoded.
+BEACON = b64decode('R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')
+
+
+ANALYTIC_SCRIPT = '''
+    """(function(){
+    var d=document,i=new Image,e=encodeURIComponent;
+    i.src='%s/a.gif?url='+e(d.location.href)+'&ref='+e(d.referrer)+'&t='+e(d.title);
+    })()""".replace('\n', '')
+'''
+
+
+
 @app.route('/a.gif')
 def analyze():
     ''' View analytics 1pixel gif '''
@@ -7,4 +29,6 @@ def analyze():
 @app.route('/a.js')
 def script():
     ''' View analytics javascript '''
-    pass
+    return Response(
+        app.config['JAVASCRIPT'] % (app.config['DOMAIN']),
+        mimetype='text/javascript')
