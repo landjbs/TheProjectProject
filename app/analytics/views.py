bd22a079e67b4f1c4ad503877d2425044cc9bf06
landjbs <landjbs@gmail.com>
2020-08-22 21:53:39 -0400

    analytics

--- app/analytics/views.py
+++ app/analytics/views.py
@@ -21,7 +21,15 @@ ANALYTIC_SCRIPT = '''
 @app.route('/a.gif')
 def analyze():
     ''' View analytics 1pixel gif '''
-    pass
+    if not request.args.get('url'):
+        abort(404)
+
+    with database.transaction():
+        PageView.create_from_request()
+
+    response = Response(app.config['BEACON'], mimetype='image/gif')
+    response.headers['Cache-Control'] = 'private, no-cache'
+    return response
 
 
 @app.route('/a.js')
