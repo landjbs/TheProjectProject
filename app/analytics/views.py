9abe5e5765bc161a650da568eae0eb6ac82af682
landjbs <landjbs@gmail.com>
2020-08-23 16:21:34 -0400

    mobile management

--- app/analytics/views.py
+++ app/analytics/views.py
@@ -10,9 +10,7 @@ def analyze():
     ''' View analytics 1pixel gif '''
     if not request.args.get('url'):
         abort(404)
-
     PageView.create_from_request()
-
     response = Response(current_app.config['BEACON'], mimetype='image/gif')
     response.headers['Cache-Control'] = 'private, no-cache'
     return response
