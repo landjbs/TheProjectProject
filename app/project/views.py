afa31b0b7f367d328ad66fbef1666ea4bf7b71c5
landjbs <landjbs@gmail.com>
2020-08-25 20:04:57 -0400

    current auth

--- app/project/views.py
+++ app/project/views.py
@@ -579,7 +579,7 @@ def remove_link(project_id, link_id):
     return redirect(request.referrer)
 
 
-@project.route('/save_instructions/<int:project_id>', methods=['POST'])
+@project.route('/save_instructions/<int:project_id>', methods=['GET'])
 def save_instructions(project_id):
     ''' Saves project instructions '''
     project = Project.query.get_or_404(project_id)
@@ -587,7 +587,10 @@ def save_instructions(project_id):
         flash('Could not edit instructions because you are not a project member.',
             category='error')
     else:
-        instructions = request.form.get('instructions')
+        print(request.json)
+        for elt in request:
+            print(elt)
+        instructions = request.json.get('data')
         print(instructions)
         if instructions is not None:
             project.instructions = instructions
