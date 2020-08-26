34a9d81c27f3edbda6354b4174e3bf25c849b4bd
landjbs <landjbs@gmail.com>
2020-08-25 21:27:03 -0400

    current auth

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-project = Blueprint('project', __name__, template_folder='templates', static_url_path='/',
-                    static_folder='static')
+project = Blueprint('project', __name__, template_folder='templates')
 
 from . import views
