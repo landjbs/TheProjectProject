053c374eb627392abecb5fac6ba914175ee5bc94
landjbs <landjbs@gmail.com>
2020-08-28 08:01:47 -0400

    collab ajax

--- app/static/js/collab.js
+++ app/static/js/collab.js
@@ -1,17 +1,17 @@
 function sendCollab(url) {
-  $.ajax({
-    type: 'POST',
-    url: url,
-    dataType: 'json',
-    success: function(data){
-      var stars = data['stars']
-      var is_starred = data['is_starred']
-      document.getElementById(count_id).innerHTML = stars + ' <i class="fa fa-star"></i>';
-      if (is_starred==0) {
-        document.getElementById(star_id).className = 'fa fa-star-o';
-      } else {
-        document.getElementById(star_id).className = 'fa fa-star';
-      }
-    }
-  })
+  // $.ajax({
+  //   type: 'POST',
+  //   url: url,
+  //   dataType: 'json',
+  //   success: function(data){
+  //     var stars = data['stars']
+  //     var is_starred = data['is_starred']
+  //     document.getElementById(count_id).innerHTML = stars + ' <i class="fa fa-star"></i>';
+  //     if (is_starred==0) {
+  //       document.getElementById(star_id).className = 'fa fa-star-o';
+  //     } else {
+  //       document.getElementById(star_id).className = 'fa fa-star';
+  //     }
+  //   }
+  // })
 }
