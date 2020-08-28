4c4cf110cf837234a5d8982242487ea1317a503a
landjbs <landjbs@gmail.com>
2020-08-28 10:51:49 -0600

    company init

--- app/company/models.py
+++ app/company/models.py
@@ -6,12 +6,18 @@ class Company(CRUDMixin, db.Model):
     __searchable__ = False
     name = db.Column(db.String(128), nullable=False, unique=True)
     code = db.Column(db.String(128), nullable=False, unique=True)
-    users = relationship('User', )
+    users = relationship('Company_Role', )
 
 
 
 
 class Company_Role(db.Model):
     __tablename__ = 'company_role'
+    # users
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    
+    user = relationship('User', back_populates='companies')
+    # company
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), primary_key=True)
+    company = relationship('Company', back_populates='users')
+    # role
+    role = db.Column(db.Text(128), nullable=True, unique=False)
