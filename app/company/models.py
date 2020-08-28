2718803c5cfa2f5ac77c26988427c38819ee390f
landjbs <landjbs@gmail.com>
2020-08-28 10:54:02 -0600

    company init

--- app/company/models.py
+++ app/company/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app.database import db, CRUDMixin
 
 
@@ -6,7 +8,10 @@ class Company(CRUDMixin, db.Model):
     __searchable__ = False
     name = db.Column(db.String(128), nullable=False, unique=True)
     code = db.Column(db.String(128), nullable=False, unique=True)
-    users = relationship('Company_Role', )
+    users = relationship('Company_Role',
+                        back_populates='companies',
+                        cascade='delete-orphan',
+                        order_by='desc(Company_Role.joined_on)')
 
 
 
@@ -21,3 +26,5 @@ class Company_Role(db.Model):
     company = relationship('Company', back_populates='users')
     # role
     role = db.Column(db.Text(128), nullable=True, unique=False)
+    # joined on
+    joined_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
