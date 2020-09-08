0536a912fcc457630313e98cbb6049a7894f7599
landjbs <landjbs@gmail.com>
2020-09-08 12:04:57 -0600

    V0.0.9 | Hover Cards

--- app/base/views.py
+++ app/base/views.py
@@ -1,12 +1,20 @@
-from flask import render_template, send_from_directory, current_app, request
+from flask import render_template, redirect, url_for
+from flask_login import current_user
 
 from ..base import base
 
 
 @base.route('/', methods=['GET'])
+def domain():
+    if current_user.is_authenticated and current_user.is_active:
+        return redirect(url_for('hub.home'))
+    return redirect(url_for('base.index'))
+
+
 @base.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 @base.route('/about', methods=['GET'])
 def about():
