2c60b67bb12011c74143089d1aafcd8f16e17ddf
landjbs <landjbs@gmail.com>
2020-09-07 20:24:33 -0600

    Company models

--- app/company/models.py
+++ app/company/models.py
@@ -3,10 +3,9 @@ from datetime import datetime
 from app.database import db, CRUDMixin
 
 
-
 class Company(CRUDMixin, db.Model):
     __tablename__ = 'company'
-    __searchable__ = False
+    # __searchable__ = ['name']
     # name
     name = db.Column(db.String(128), nullable=False, unique=True)
     # code for url
@@ -39,3 +38,16 @@ class Company_Role(db.Model):
     role = db.Column(db.Text(128), nullable=True, unique=False)
     # joined on
     joined_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class Company_Project(db.Model):
+    __tablename__ = 'company_project'
+    # project
+    project_id = db.Column(db.init_app, db.ForeignKey('project.id'), primary_key=True)
+    project = relationship('Project', back_populates='projects')
+    # company
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), primary_key=True)
+    company = relationship('Company', back_populates='users')
+    # pay
+    pay = db.Column(db.Integer, nullable=False, default=0)
+    
