cda74ac9358c2e87eee389612a5558d852f51900
landjbs <landjbs@gmail.com>
2020-09-09 13:03:56 -0600

    V0.1.0 | Company

--- app/company/models.py
+++ app/company/models.py
@@ -28,23 +28,23 @@ class Company(CRUDMixin, db.Model):
     last_active = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
-class Company_Role(db.Model):
-    __tablename__ = 'company_role'
-    # users
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    user = relationship('User', back_populates='companies')
-    # company
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), primary_key=True)
-    company = relationship('Company', back_populates='users')
-    # joined on
-    joined_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+# class Company_Role(db.Model):
+#     __tablename__ = 'company_role'
+#     # users
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+#     user = relationship('User', back_populates='companies')
+#     # company
+#     company_id = db.Column(db.Integer, db.ForeignKey('company.id'), primary_key=True)
+#     company = relationship('Company', back_populates='users')
+#     # joined on
+#     joined_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
-class Company_Project(db.Model):
-    __tablename__ = 'company_project'
-    # project
-    project_id = db.Column(db.init_app, db.ForeignKey('project.id'), primary_key=True)
-    project = relationship('Project', back_populates='projects')
-    # company
-    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), primary_key=True)
-    company = relationship('Company', back_populates='users')
+# class Company_Project(db.Model):
+#     __tablename__ = 'company_project'
+#     # project
+#     project_id = db.Column(db.init_app, db.ForeignKey('project.id'), primary_key=True)
+#     project = relationship('Project', back_populates='projects')
+#     # company
+#     company_id = db.Column(db.Integer, db.ForeignKey('company.id'), primary_key=True)
+#     company = relationship('Company', back_populates='users')
