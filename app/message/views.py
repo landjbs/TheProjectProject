b3576ad1725a68ae573aa6dd531a7ab8799e49dc
landjbs <landjbs@gmail.com>
2020-09-16 12:08:49 -0600

    V0.3.0 | Messaging

--- app/message/views.py
+++ app/message/views.py
@@ -9,8 +9,5 @@ from ..message import message
 @message.route('/messages')
 @login_required
 def messages():
-    # current_user.send_message('Bap bop boop!!!', to=[current_user])
-    for uc in current_user.channels:
-        for message in uc.channel.messages:
-            print(message)
+    current_user.send_message('Bap bop boop!!!', to=[current_user])
     return render_template('messages.html')
