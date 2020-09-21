0cd4a9583ca57d0f68bdcfe351401f30d121e456
landjbs <landjbs@gmail.com>
2020-09-21 17:28:30 -0600

    message polling

--- app/message/views.py
+++ app/message/views.py
@@ -20,11 +20,18 @@ def messages():
 @login_required
 def check_messages():
     since = request.args.get('since', 0.0, type=float)
+    # channels = current_user.channels.query.filter
     messages = current_user.messages.filter(
                 Message.timestamp > since
             ).order_by(Message.timestamp.asc())
-    print(messages)
-    return messages
+    print(messages.all())
+    # html = render_message(message, message_data, sent_by_me=True)
+    # render_message = get_template_attribute(
+    #                     'macros/chat.html', 'render_message'
+    #                 )
+    # message_data = {'last_sent' : channel.messages[1].timestamp}
+    # html = render_message(message, message_data, sent_by_me=True)
+    return jsonify()
 
 
 @message.route('/open_single_channel', methods=['POST'])
