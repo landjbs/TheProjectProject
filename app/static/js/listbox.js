1f91f15ce4652171a148c91e2868d6e358d6a030
landjbs <landjbs@gmail.com>
2020-09-28 14:47:03 -0400

    listbox ajax

--- app/static/js/listbox.js
+++ app/static/js/listbox.js
@@ -1,3 +1,16 @@
+// TASKS
+function change_task_status(project_id, task_id, action) {
+  // build endpoint for editing task
+  url = Flask.url_for(
+          'project.change_task_status',
+          'project_id': project_id
+          'task_id'   : task_id
+          'action'    : action
+  );
+}
+
+
+// COMMENTS
 function delete_comment(project_id, comment_id) {
   url = Flask.url_for(
           'project.delete_comment',
