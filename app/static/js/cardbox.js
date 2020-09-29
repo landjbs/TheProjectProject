175bdb74a7947e836f6d554934d08acf446d3e75
landjbs <landjbs@gmail.com>
2020-09-29 15:23:34 -0400

    listelt ajax

---
+++ app/static/js/cardbox.js
@@ -0,0 +1,25 @@
+// QUESTIONS
+// ask question
+
+// TODO: move edit_answer listener here from macros/cards/question.html
+
+// delete question
+// TODO: consolidate into single delete function
+function delete_question(project_id, question_id) {
+  url = Flask.url_for(
+          'project.delete_question',
+          {
+            'project_id'    :   String(project_id),
+            'question_id'   :   String(question_id)
+          }
+  );
+  $.ajax(url).done(
+    function (payload) {
+      if (payload['success']==true) {
+        document.getElementById('question-' + question_id).remove();
+      } else {
+        alert('Could not delete.')
+      }
+    }
+  );
+}
