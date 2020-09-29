40c8785eba7a882825afc35762c1d82153bb1c3d
landjbs <landjbs@gmail.com>
2020-09-29 13:36:39 -0400

    listelt ajax

--- app/static/js/listbox.js
+++ app/static/js/listbox.js
@@ -29,7 +29,10 @@ function change_task_status(project_id, task_id, action) {
 function delete_comment(project_id, comment_id) {
   url = Flask.url_for(
           'project.delete_comment',
-          {'project_id':String(project_id), 'comment_id':String(comment_id)}
+          {
+            'project_id':String(project_id),
+            'comment_id':String(comment_id)
+          }
   );
   $.ajax(url).done(
     function (payload) {
