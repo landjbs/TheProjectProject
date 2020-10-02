484d7c77dc52a266042408893f10f00a9ba03e4b
landjbs <landjbs@gmail.com>
2020-10-02 12:29:15 -0400

    add template migration

--- app/add/forms.py
+++ app/add/forms.py
@@ -18,8 +18,25 @@ class Add(BaseForm):
     Dynamic add form. Currently supports Project and Company
     '''
     ## SHARED FIELDS ##
-    name = StringField(label='Name',
+    name = StringField(label='Project Name',
                        validators=[DataRequired(), Length(1, 40)],
                        description='Give your project a name!',
                        render_kw={'placeholder':'TheProjectProject',
                                   'max': 30})
+    oneliner = StringField(label='One-Liner',
+                           validators=[DataRequired(), Length(1, 100)],
+                           description='One line description of your project.',
+                           render_kw={'placeholder':'The platform for sharing, collaborating on, and publicizing independent projects.',
+                                      'max': 40})
+    summary = TextAreaField(label='Summary',
+                        validators=[DataRequired(), Length(1, 400)],
+                        description='Describe your project in more detail.',
+                        render_kw={'placeholder': ('TheProjectProject is a virtual community of innovators, who collaborate on projects across a wide range of fields and time windows. '
+                                                'I need a team of web developers, database experts, and creative minds to help me build this platform. We will tackle interesting problems '
+                                                'such as matching users with projects they will like and designing an interface that helps people communicate their ideas...'),
+                                  'max':400})
+    subjects = SelectMultipleField('Subjects',
+                                    description=('What subjects might this '
+                                                'project involve?'),
+                                    validators=[], choices=[], coerce=int)
+    
