6e4014a9333335840b9732f4ee55ec8823b0dd46
landjbs <landjbs@gmail.com>
2020-10-03 10:29:04 -0400

    form javascript

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -1,5 +1,5 @@
 function field_char_counter(field) {
   var char_num = field.value.length;
-  counter = field.getElementById('char_inc' + field.id);
+  counter = document.getElementById('char_inc' + field.id);
   counter.innerHTML = char_num;
 }
