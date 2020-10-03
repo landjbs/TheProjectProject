3cfc6c052439ddc3ce49840f54e4893bdb9a2f09
landjbs <landjbs@gmail.com>
2020-10-03 10:31:56 -0400

    form javascript

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -1,5 +1,10 @@
+// STRING FIELDS
+// takes this object of field and updates character count
 function field_char_counter(field) {
   var char_num = String(field.value.length);
   counter = document.getElementById('char_inc_' + field.id);
   counter.innerHTML = char_num;
 }
+
+
+// INT FIELDS
