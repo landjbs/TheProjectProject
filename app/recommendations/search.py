7e7cf79453f7bfb0327352b37abc2815a1bd762a
landjbs <landjbs@gmail.com>
2020-10-07 15:44:17 -0400

    company search

--- app/recommendations/search.py
+++ app/recommendations/search.py
@@ -8,19 +8,33 @@ from app.utils import partition_query
 
 def text_search(search_text):
     ## results ##
-    project_results = Project.query.filter(Project.name.contains(search_text) |
-                                   Project.oneliner.contains(search_text))
-    user_results = User.query.filter(User.name.contains(search_text) |
-                                     User.about.contains(search_text))
-    subject_results = Subject.query.filter(Subject.name.contains(search_text))
+    project_results = Project.query.filter(
+        Project.name.contains(search_text) |
+        Project.oneliner.contains(search_text)
+    )
+    user_results = User.query.filter(
+        User.name.contains(search_text) |
+        User.about.contains(search_text)
+    )
+    subject_results = Subject.query.filter(
+        Subject.name.contains(search_text)
+    )
+    company_results = Company.query.filter(
+        Company.name.contains(search_text) |
+        Company.oneliner.contains(search_text)
+    )
     ## analytics ##
     project_count = project_results.count()
     user_count = user_results.count()
     subject_count = subject_results.count()
+    company_count = company_results.count()
     ## limits ##
     project_results = project_results.limit(30)
     user_results = user_results.limit(30)
+    company_results = company_results.limit(30)
     ## return dict of results and analytics
-    return {'project'       :   (project_results.all(),     project_count),
-            'user'          :   (user_results.all(),        user_count),
-            'subject'       :   (subject_results.all(),     subject_count)}
+    return {
+        'project'       :   (project_results.all(),     project_count),
+        'user'          :   (user_results.all(),        user_count),
+        'subject'       :   (subject_results.all(),     subject_count),
+    }
