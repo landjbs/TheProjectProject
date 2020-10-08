f664a4382f2eb397e567c436942b8338c1ece0a3
landjbs <landjbs@gmail.com>
2020-10-08 18:06:16 -0400

    form finishingg

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -269,6 +269,7 @@ function set_type(type) {
 
 function hide_class(cls) {
   document.querySelectorAll('.' + cls).forEach(function(el) {
+    console.log(el.classList);
     el.classList.add('hidden');
     // // TODO: used clone to save classes so can go back
     // el.remove();
@@ -277,8 +278,9 @@ function hide_class(cls) {
 
 
 function show_class(cls) {
-  document.querySelector('.' + cls).forEach(function(el) {
+  document.querySelectorAll('.' + cls).forEach(function(el) {
     if (el.classList.contains(cls)) {
+      console.log(el.classList);
       el.classList.remove(cls);
     }
   });
